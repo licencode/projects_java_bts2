2ad8510d22fecb9d746e7327fa1536051cb3a409
licencode <licencode@gmail.com>
2020-11-03 16:11:21 +0100

    creation class mainPersonne et Creation d'une personne

---
+++ ProjTest/src/personnes/MainPersonne.java
@@ -0,0 +1,11 @@
+package personnes;
+
+public class MainPersonne {
+
+	public static void main(String[] args) {
+
+		// Creation d'une personne avec le 1er constructeur
+		Personne personne1 = new Personne("Dupont","Paul", 30);
+	}
+
+}
