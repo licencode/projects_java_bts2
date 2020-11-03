7848422217a12cf01821e8cc1903d4442d729385
licencode <licencode@gmail.com>
2020-11-03 11:49:27 +0100

    Operation sur les comptes

--- ProjTest/src/Comptes/MainCompte.java
+++ ProjTest/src/Comptes/MainCompte.java
@@ -8,9 +8,12 @@ public class MainCompte {
 		Compte cpt1 = new Compte();
 		
 		// Creation d'un compte avec un numero de compte et un solde initial
-		Compte cpt2 = new Compte(1,200);
+		Compte cpt2 = new Compte(1,200.5f);
 
-
+		// Operation sur les comptes 
+		cpt1.consulter();
+		cpt2.consulter();
+		
 	}
 
 }
