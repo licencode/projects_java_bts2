351aed263852d185010405cce94318a6bae704f8
licencode <licencode@gmail.com>
2020-11-03 11:02:19 +0100

    cree methode consulter

--- ProjTest/src/Comptes/Compte.java
+++ ProjTest/src/Comptes/Compte.java
@@ -5,5 +5,18 @@ public class Compte {
 	// Attributs
 	private int numCompte;
 	private float solde;
+	
+	// Methodes
+	public void crediter(float somme) {
+		solde = solde + somme;
+	}
+	
+	public void debiter(float somme) {
+		solde = solde - somme;
+	}
+	
+	public void consulter() {
+		System.out.println("Le solde est de " + solde);
+	}
 
 }
