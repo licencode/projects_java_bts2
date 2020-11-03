33949678604423edcebe9382d61705c2d17a8220
licencode <licencode@gmail.com>
2020-11-03 16:19:59 +0100

    creation 2eme personne et printer les personnes

--- ProjTest/src/personnes/MainPersonne.java
+++ ProjTest/src/personnes/MainPersonne.java
@@ -5,7 +5,13 @@ public class MainPersonne {
 	public static void main(String[] args) {
 
 		// Creation d'une personne avec le 1er constructeur
-		Personne personne1 = new Personne("Dupont","Paul", 30);
+		Personne personne1 = new Personne("Dupont", "Paul", 30);
+		
+		// Creation d'une personne avec le 2eme constructeur
+		Personne personne2 = new Personne("Bond", "James");
+		
+		System.out.println("Nom est: " + personne1.getNom() + ", Prenom est: " + personne1.getPrenom() + ", age " + personne1.getAge());
+		System.out.println("Nom est: " + personne2.getNom() + ", Prenom est: " + personne2.getPrenom());
 	}
 
 }
