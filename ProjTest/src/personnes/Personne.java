629fe7c3916e5300e1dabf675babfdbe084749f0
licencode <licencode@gmail.com>
2020-11-03 16:00:42 +0100

    creation construction et getters setters

--- ProjTest/src/personnes/Personne.java
+++ ProjTest/src/personnes/Personne.java
@@ -8,6 +8,38 @@ public class Personne {
 	private int age;
 	
 	//Constructeurs
+	public Personne(String nomPersonne, String prenomPersonne, int agePersonne) {
+		
+		nom = nomPersonne;
+		prenom = prenomPersonne;
+		age = agePersonne;
+	}
+	
+	// Getters et Setters
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 	
 
 }
