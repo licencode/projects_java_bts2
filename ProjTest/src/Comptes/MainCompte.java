37f27f767cc6f801c1c9eeedd63961588395fe3b
licencode <licencode@gmail.com>
2020-11-03 11:27:48 +0100

    Creation d'un compte vide

--- ProjTest/src/Comptes/MainCompte.java
+++ ProjTest/src/Comptes/MainCompte.java
@@ -3,7 +3,10 @@ package Comptes;
 public class MainCompte {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Creation d'un compte vide
+		Compte cpt1 = new Compte();
+		
 
 	}
 
