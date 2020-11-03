48213c6dfc2035c58d72d099cbbba6861f9801d2
licencode <licencode@gmail.com>
2020-11-03 16:07:50 +0100

    creation getter and setter pour atributs et commenter ça.

--- ProjTest/src/personnes/Personne.java
+++ ProjTest/src/personnes/Personne.java
@@ -20,9 +20,39 @@ public class Personne {
 		this.nom = nomPersonne;
 		this.prenom = prenomPersonne;
 	}
-	
-	
 
+	// Getter pour nom
+	public String getNom() {
+		return nom;
+	}
+	
+	// Setter pour nom
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	// Getter pour prenom
+	public String getPrenom() {
+		return prenom;
+	}
+	
+	// Setter pour prenom
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	// Getter pour age
+	public int getAge() {
+		return age;
+	}
+	
+	// Setter pour age
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
 	
 	
 
