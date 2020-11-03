094b2e5b312095f0e16d09955a30e5671eed07de
licencode <licencode@gmail.com>
2020-11-03 11:43:01 +0100

    Creation d'un compte avec un numero de compte et un solde initial

--- ProjTest/src/Comptes/MainCompte.java
+++ ProjTest/src/Comptes/MainCompte.java
@@ -7,6 +7,9 @@ public class MainCompte {
 		// Creation d'un compte vide
 		Compte cpt1 = new Compte();
 		
+		// Creation d'un compte avec un numero de compte et un solde initial
+		Compte cpt2 = new Compte(1,200);
+
 
 	}
 
