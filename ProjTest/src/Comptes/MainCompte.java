4da22324b5ab1247001a755998e2c0aeeecf8b4d
licencode <licencode@gmail.com>
2020-11-03 12:22:55 +0100

    ajoute solde pour compte cpt1

--- ProjTest/src/Comptes/MainCompte.java
+++ ProjTest/src/Comptes/MainCompte.java
@@ -5,10 +5,10 @@ public class MainCompte {
 	public static void main(String[] args) {
 		
 		// Creation d'un compte vide
-		Compte cpt1 = new Compte();
+		Compte cpt1 = new Compte(1,150);
 		
 		// Creation d'un compte avec un numero de compte et un solde initial
-		Compte cpt2 = new Compte(1,200.5f);
+		Compte cpt2 = new Compte(2,200.5f);
 
 		// Operation sur les comptes 
 		cpt1.consulter();
